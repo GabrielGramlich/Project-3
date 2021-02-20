559810c2b9c5663bbb3129f0995331e94e4042cf
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-02-19 20:24:22 -0600

    Add comment

--- ui.py
+++ ui.py
@@ -11,6 +11,11 @@ def display_message():
 
 
 def refresh(lines):
+	'''This method moves the line of the terminal being printed on up via the special character written,
+	  and then prints blank spaces along the the previously written lines to wipe them out, and then moves
+	  the cursor back up to begin a new series of prints. The 'lines' argument sent should be the amount
+	  of lines printed by the last section of output, placing the cursor after the last command sent to
+	  the terminal with only blank lines after.'''
 	for i in range(lines):
 		sys.stdout.write("\033[F") # Cursor up one line
 	for i in range(lines):
