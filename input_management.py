82a99a9df2ba4222cd2d55b97228068a8b8bcc1a
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-02-19 21:18:35 -0600

    Get yes/no input

--- input_management.py
+++ input_management.py
@@ -1,4 +1,17 @@
-def get_input(message):
+def get_string(message):
 	selection = input(message).lower()
 
 	return message
+
+
+def get_bool(message,true,false):
+	selection = validate_bool(true,false,input(message).lower(),message)
+	if selection == true:
+		return True
+	else:
+		return False
+
+
+	return message
+
+
