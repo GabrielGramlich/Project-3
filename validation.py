9b163bd4ddb3dadfe2662ca0ecf73a5cea9c6f6a
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-02-19 21:18:11 -0600

    Validate yes/no input

--- validation.py
+++ validation.py
@@ -1,2 +1,18 @@
+from ui import display_message
+from input_management import get_string
+
 def menu_selection_exists():
 	pass
+
+
+def invalid_bool(true,false,selection,message):
+	while True:
+		if selection == true:
+			break
+		elif selection == false:
+			break
+		else:
+			display_message(f'Invalid selection. Please select {true} or {false}.')
+			selection = get_string(message)
+
+	return selection
