acbe041246fde5392e44c0c10ebaba60b4cb7a53
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-02-19 19:54:08 -0600

    Add db management methods

---
+++ database_management.py
@@ -0,0 +1,30 @@
+def initialize_database():
+	pass
+
+
+def intialize_artist_table():
+	pass
+
+
+def intialize_artwork_table():
+	pass
+
+
+def create_row():
+	pass
+
+
+def read_row():
+	pass
+
+
+def read_rows():
+	pass
+
+
+def update_row():
+	pass
+
+
+def delete_row():
+	pass
