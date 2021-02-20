9abafbe1e64b48a01c29d16c01bee63d1f42443b
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-02-19 20:30:35 -0600

    Let user select from menu

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from ui import display_options
+from input_management import get_input
+
 def main():
 	selection = ''
-	while selection != 'exit':
-		pass
+	display_options()
+	selection = get_input('\nWhich option would you like? ')
+
 
 
 if __name__ == '__main__':
