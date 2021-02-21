be2bf6f057c7049e0f8c08943c6be8ff0640bb84
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-02-21 16:55:02 -0600

    Add update methods

--- database/artist.py
+++ database/artist.py
@@ -6,5 +6,13 @@ class Artist:
 		self.db_id = db_id
 
 
+	def update_name(self,new_name):
+		self.name = new_name
+
+
+	def update_email(self,new_email):
+		self.email = new_email
+
+
 	def __str__(self):
 		return f'Artist: {self.name}\nEmail: {self.email}'
