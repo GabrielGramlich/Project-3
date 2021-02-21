4192c3ecce17e388d6cea717f36e89df49279445
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-02-21 10:07:39 -0600

    Refactor

--- user/ui.py
+++ user/ui.py
@@ -1,3 +1,20 @@
+def get_string(message):
+	selection = input(message).lower()
+
+	return message
+
+
+def get_bool(message,true,false):
+	selection = validate_bool(true,false,get_string(message).lower(),message)
+	if selection == true:
+		return True
+	else:
+		return False
+
+
+	return message
+
+
 def display_options(menu):
 	print(str(menu))
 
