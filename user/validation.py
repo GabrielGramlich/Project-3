a3d5cba2c49d83bb0506747d8e4313204cde944a
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-02-21 10:02:14 -0600

    Validate menu selection

--- user/validation.py
+++ user/validation.py
@@ -1,8 +1,17 @@
 from ui import display_message
 from input_management import get_string
 
-def menu_selection_exists():
+def menu_selection_exists(menu, selection):
+	return is_int(selection) or menu.is_valid(selection) or selection = 'x'
 	pass
+
+
+def is_int(test_int):
+	try:
+		good_int = int(test_int)
+		return True
+	except ValueError:
+		return False
 
 
 def invalid_bool(true,false,selection,message):
