4469fe78bb779d939316b7ef97b865161fd4c5b4
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-02-21 10:41:45 -0600

    Refactor

---
+++ crud.py
@@ -0,0 +1,46 @@
+def create_artist():
+	pass
+
+
+def display_artist():
+	pass
+
+
+def delete_artist():
+	pass
+
+
+def update_artist_name():
+	pass
+
+
+def update_email():
+	pass
+
+
+def create_artwork():
+	pass
+
+
+def display_artwok():
+	pass
+
+
+def delete_artwork():
+	pass
+
+
+def update_artist():
+	pass
+
+
+def update_artwork_name():
+	pass
+
+
+def update_price():
+	pass
+
+
+def update_avilability():
+	pass
--- main.py
+++ main.py
@@ -86,53 +86,5 @@ def initialize_artwork_menu():
 	return artwork_menu
 
 
-def create_artist():
-	pass
-
-
-def display_artist():
-	pass
-
-
-def delete_artist():
-	pass
-
-
-def update_artist_name():
-	pass
-
-
-def update_email():
-	pass
-
-
-def create_artwork():
-	pass
-
-
-def display_artwok():
-	pass
-
-
-def delete_artwork():
-	pass
-
-
-def update_artist():
-	pass
-
-
-def update_artwork_name():
-	pass
-
-
-def update_price():
-	pass
-
-
-def update_avilability():
-	pass
-
-
 if __name__ == '__main__':
 	main()
